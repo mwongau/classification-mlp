d7a2dfee7163591acc593c2906abc82f08952df4
M Wong <wongmt15@gmail.com>
2022-06-14 01:52:37 +1000

    14-6

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-## Classification of survival for Titanic dataset
+## Classification of survival of a passenger in Titanic dataset using MLP
 
 ### Dataset used
-The Titanic dataset is loaded as a Pandas DataFrame from Seaborn. 
+The Titanic dataset is loaded as a Pandas DataFrame by Seaborn. 
 Reference for Seaborn datasets:
 https://seaborn.pydata.org/generated/seaborn.load_dataset.html
 
 ### Test condition
 Only the numerical features of Titanic dataset are used in classification.
-80% of the dataset is used as training data & 20% as test data.
-Ten different partitions of the Titanic data into training & test data are used.
-A multi-layer perceptron is used to determine the classification accuracy of survival on the 
-test set for each partition.
+10-fold cross-validation is used to determine the classification accuracy of survival.
+The classifier used is multi-layer perceptron (MLP). 
 
 ### Test result
-The average of the classification accuracies of the 10 different partitions is 70.2%
+The 10-fold cross-validation accuracy is 72.00%.
 The accuracy might be further improved by using more features instead of using numerical features 
 only. 
   
--- titanic.py
+++ titanic.py
@@ -1,28 +1,22 @@
-# To classify survival of Titanic dataset by MLP
+# To find classification accuracy of survival of a passenger in Titanic dataset by MLP using 10-fold cross-validation
 
 import seaborn as sns
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 
 # Titanic dataset is loaded by Seaborn.
 # reference: https://seaborn.pydata.org/generated/seaborn.load_dataset.html 
 titanic=sns.load_dataset('titanic')
 print(titanic.info())
-print(titanic.head())
 df = titanic.copy()
-data= df.drop(columns=['sex', 'class', 'deck', 'embarked', 'embark_town', 'alone', 'who', 'adult_male', 'alive'])
+data= df.drop(columns=['sex', 'class', 'deck', 'embarked', 'embark_town', 'alone', 'who', 'adult_male', 'alive']) # use numerical features only
 data = data.dropna() # remove rows with missing values 
 print('length of dataset after pre-processing', len(data))
 print(data.info())
+print(data.describe())
 labels = data.pop('survived')
-train_data, test_data, y_train, y_test= train_test_split(data,labels,test_size=0.20,random_state=10)
           
-model = MLPClassifier(max_iter=300)
-model.fit(train_data, y_train)
-score = model.score(test_data, y_test)
-print('length of train & test data', len(train_data), len(test_data))
-print('score', score)
-
-
-
+model = MLPClassifier(max_iter=300, random_state=42, solver='lbfgs')
+scores = cross_val_score(model, data, labels, cv=10) # 10-fold cross-validation
+print("10-fold cross-validation accuracy= %.4f, std= %.4f" % (scores.mean(), scores.std()))
